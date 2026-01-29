0d7d0e598c096a28fb083c160e8209008b38eb8e
Shaun Koehne <sidkid78@gmail.com>
2026-01-29 16:36:47 -0600

    config: Update Next.js output file tracing exclusions to include new platform-specific binaries and remove lightningcss-win32-x64-msvc.

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   outputFileTracingExcludes: {
     '*': [
       'node_modules/@swc/core-linux-x64-gnu',
       'node_modules/@swc/core-linux-x64-musl',
       'node_modules/@esbuild/linux-x64',
+      'node_modules/@esbuild/win32-x64',
       'node_modules/terser',
       'node_modules/webpack',
-      'node_modules/lightningcss-win32-x64-msvc',
+      'node_modules/sharp',
+      'node_modules/@next/swc-linux-x64-gnu',
+      'node_modules/@next/swc-linux-x64-musl',
+      'node_modules/@next/swc-win32-x64-msvc',
     ],
   },
 };
