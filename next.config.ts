10737f5dddc01a14bab3c09c7be944615ccfbcf5
Shaun Koehne <sidkid78@gmail.com>
2026-01-29 16:23:58 -0600

    build: Exclude specific ode_modules packages from Next.js output file tracing.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+      'node_modules/terser',
+      'node_modules/webpack',
+      'node_modules/lightningcss-win32-x64-msvc',
+    ],
+  },
 };
 
 export default nextConfig;
